8567452683eaf9c3da9f7daf60b53384ac446ea1
moroviintaas <moroviintaas@gmail.com>
2023-05-23 20:06:59 +0200

    Moved fuzzy_card_set out of crate.

--- src/hand/mod.rs
+++ src/hand/mod.rs
@@ -1,12 +1,10 @@
 mod card_set_gen;
 mod card_set;
 mod r#trait;
-#[cfg(feature = "fuzzy")]
-mod fuzzy_card_set;
 
 pub use card_set::*;
 pub use card_set_gen::*;
 pub use r#trait::*;
-#[cfg(feature = "fuzzy")]
-pub use fuzzy_card_set::*;
+//#[cfg(feature = "fuzzy")]
+//pub use fuzzy_card_set::*;
 
