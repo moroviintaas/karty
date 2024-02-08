313ccfe7e893d2fbcc0d828db562acef32de32e2
moroviintaas <moroviintaas@gmail.com>
2024-02-08 17:39:25 +0100

    Added proc_macro crate to repository.

---
+++ karty_proc_macro/README.md
@@ -0,0 +1,4 @@
+# karty_proc_macro
+
+This crate is meant to provide macros for [`karty`](https://crates.io/crates/karty_proc_macro).
+Currently over one macro is provided however more will be introduced later.
---
+++ karty_proc_macro/src/lib.rs
@@ -0,0 +1,20 @@
+use quote::quote;
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use syn::parse_macro_input;
+use syn::DeriveInput;
+
+#[proc_macro_derive(RandomSymbol)]
+pub fn implement_distribution_of_dimension(item: TokenStream) -> TokenStream{
+    let ast = parse_macro_input!(item as DeriveInput);
+    //let vis = ast.vis;
+    let ident = ast.ident;
+    let expanded = quote! {
+        impl Distribution<#ident> for Standard{
+            fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> #ident{
+                #ident::from_usize_index(rng.gen_range(0..#ident::SYMBOL_SPACE)).unwrap()
+            }
+        }
+    };
+    TokenStream::from(expanded)
+}
