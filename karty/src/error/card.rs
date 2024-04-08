994dd244903035b44ebde54880463c13f41dbb6a
moroviintaas <moroviintaas@gmail.com>
2024-04-08 16:47:23 +0200

    Reformatted errors with thiserror crate

--- karty/src/error/card.rs
+++ karty/src/error/card.rs
@@ -4,22 +4,22 @@
 //! # Authors:
 //! [morovintaas](mailto:moroviintaas@gmail.com)
 //!
-use std::fmt::{Display, Formatter};
 
 /// Type used for indicating errors related with operations defined in crate
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum CardError{
     /// Instance indicating that wrong position for [`Suit`][crate::suits::Suit] was used, associated value is violating value.
+    #[error("Wrong suit index was used, associated value {0} is violating rules")]
     WrongSuitPosition(usize),
     /// Instance indicating that wrong position for [`Figure`][crate::figures::Figure] was used, associated value is violating value.
+    #[error("Wrong figure index was used, associated value {0} is violating rules")]
     WrongFigurePosition(usize),
+    #[error("Wrong index  was used, associated value {0} is violating rules")]
     WrongPosition(usize),
+    #[error("Wrong mask format")]
     WrongMaskFormat,
+    #[error("Mask space violated")]
     MaskSpaceViolated,
+    #[error("Parse error")]
     ParseError
-}
-impl Display for CardError{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{self:?}")
-    }
 }
--- karty/src/error/card_set.rs
+++ karty/src/error/card_set.rs
@@ -6,13 +6,21 @@ use crate::symbol::CardSymbol;
 #[derive(Debug, Clone, PartialEq, thiserror::Error)]
 #[cfg_attr(feature = "speedy", derive(Writable, Readable))]
 pub enum CardSetErrorGen<Crd: CardSymbol>{
+    #[error("Card {0:?} was not in hand")]
     CardNotInHand(Crd),
+    #[error("Hand is empty")]
     EmptyHand,
+    #[error("Hand is full")]
     HandFull,
+    #[error("Card duplicate found")]
     CardDuplicated(Crd),
+    #[error("Hand is not initialised yes")]
     HandNotInitialised,
+    #[error("Difference of lengths found: {0} and {1}")]
     DifferentLengths(usize, usize),
+    #[error("Parse error")]
     ParseError,
+    #[error("Card sets are not equal (but they were expected to be). Expected: {expected:?}, found: {found:?}")]
     ExpectedEqualCardSets{
         expected: Vec<Crd>,
         found: Vec<Crd>
